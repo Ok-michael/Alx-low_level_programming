a898b7f769df54984552298738281af911b24138
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 09:06:21 -0700

    corrections to match betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,18 +7,18 @@
  * assign a random number to n each time the program is run
  * finally checks if the number is positive, negative or zero
  * and prints a corresponding message.
+ * return: alway return (0) when the program runs successfully
  */
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if ( n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if ( 0 > n)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
