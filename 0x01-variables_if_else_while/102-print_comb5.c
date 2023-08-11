3cf8de7b78227953d602502c390ebd17d94f141d
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 16:23:05 -0700

    create 102-print_comb3.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-    int num1, num2;
-
-    for (num1 = 0; num1 <= 99; num1++) 
-    {
-        for (num2 = num1; num2 <= 99; num2++) 
+	int num1, num2
+	
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-            if (num1 != num2) 
-	    {
-                putchar('0' + num1 / 10);
-                putchar('0' + num1 % 10);
-                putchar(' ');
-                putchar('0' + num2 / 10);
-                putchar('0' + num2 % 10);   
-                if (num1 != 98 || num2 != 99)
-	       	{
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+		for (num2 = num1; num2 <= 99; num2++)
+		{
+			if (numq != num2)
+			{
+				putchar('0' + num1 / 10);
+				putchar('0' + num1 % 10);
+				putchar(' ');
+				putchar('0' + num2 / 10);
+				putchar('0' + num2 % 10);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	put('\n');
+	return (0);
 }
