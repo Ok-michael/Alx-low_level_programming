29a5967e9df983c4ab4985912a1db62efc7ab34f
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 09:10:08 -0700

    syntax corrections on 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,12 @@
  * assign a random number to n each time the program is run
  * finally checks if the number is positive, negative or zero
  * and prints a corresponding message.
- * return: alway return (0) when the program runs successfully
+ * Return: alway return (0) when the program runs successfully
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
