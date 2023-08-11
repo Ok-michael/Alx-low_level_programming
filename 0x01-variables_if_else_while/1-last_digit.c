d3e2967c7faa7f152e51500def50bd31efd1bb7d
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 09:49:30 -0700

    syntax fixing on 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  main - declares and integer n and assignes it a new value each time it's run
+ *  main - declares and integer n and assignes it a random new value
  *  declare two other variables of type in and char* for processing
  *  and geting the last digits of n each time
  *  finally prints a message combining bothe the values of n, lst_dgt
@@ -16,10 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lst_dgt = n % 10;
-
 	if (lst_dgt > 5)
-		printf("Last digit of %d is %d and is greater than 5\n",n, lst_dgt);
-	else if(lst_dgt <=5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lst_dgt);
+	else if(lst_dgt <= 5 && lst_dgt != 0)
 		printf("Last digit of %d is %d and iss less than 6 and not 0\n", n, lst_dgt);
 	else if (lst_dgt == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lst_dgt);
