d0afa89798c49474ca43bbc6d9bbf2f1141e1dcb
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 12:10:43 -0700

    create r-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Declare a variable sm_ltr and initializes it to 'a'
+ * uses a while loop to modify the values of
+ * and print it out using putchar if the value is not either 'e' or 'q'
+ * Return: if everything goes well, the funtion returns '0'
+ */
+int main(void)
+{
+	char sm_ltr = 'a';
+
+	while (sm_ltr <= 'z')
+	{
+		sm_ltr++;
+		if (sm_ltr != ('e' || 'q')
+				{
+					putchar(sm_ltr);
+				}	
+	}
+	return (0);
+}
