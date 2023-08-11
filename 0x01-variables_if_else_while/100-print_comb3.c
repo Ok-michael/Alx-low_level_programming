6fa8e00da78832ed2e8c8987b43aa5c33e92e867
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 13:33:20 -0700

    create 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
  */
 int main()
 {
-    for (int m = 0; m < 10; m++)
+    for (int i = 0; i < 10; i++)
     {
-        for (int n = m + 1; n < 10; n++)
+        for (int j = i + 1; j < 10; j++)
        	{
-            putchar('0' + m);
-            putchar('0' + n);
-            if (m < 8 || n < 9)
+            putchar('0' + i);
+            putchar('0' + j);
+            if (i < 8 || j < 9)
 	    {
                 putchar(',');
                 putchar(' ');
