9a9d4ae1e5bfc6d44cc81ae3d950282749796d58
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 18:51:37 -0700

    create 7-tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main(void)
 /**
  * main -this funciton prints the lowercase alphabet in reverse order
  * using a while loop and variable Char.
  * the function prototype is `int main(void)`
  * Return: it returns 0 when ran successfully
  */
+int main(void)
 {
 	char Char = 'z';
 
