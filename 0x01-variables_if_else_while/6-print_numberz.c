9f3dd716536ae553bb2a5e96a263b1ad1f5a9e6d
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 18:17:01 -0700

    modify 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,18 +4,17 @@
  * main - This function prints all the single digit numbers
  * of base 10 starting from 0 followed by a new line.
  * it declares a variable Char of type char , initializes it 
- * to '0' and uses a while loop to modify the value of Char.
+ * to '0' and uses a for loop to modify the value of Char.
  * Return: the funtion returns 0 each time this is successfully
  * ran
  */
 int main(void)
 {
-	char Char = '0';
-
-	while (Char <= '9')
+	int card_char = 48, cnt;
+	
+	for(cnt 0; cnt <10; cnt++)
 	{
-		putchar(Char);
-		Char ++;
+		putchar(48 + cnt);
 	}
 	putchar('\n');
 	return (0);
