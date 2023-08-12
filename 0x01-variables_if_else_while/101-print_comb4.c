1eff3811afabb6f0d7f788468e364029f63e7a66
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-11 17:05:25 -0700

    modify 101-print_comb3.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,9 +19,8 @@ int main(void)
 				putchar('0' + p);
 				putchar('0' + q);
 				putchar('0' + r);
-				putchar(',' + ' ');
 			}
-				if (p < 7 || (p == 7 && q < 8) || (p == 7 && q == 8 && r < 9))
+				if (p != 7 || q != 8 || r != 9)
 				{
 					putchar(',');
 					putchar(' ');
