5083971502026d19dc7d0a194668316a40c70c64
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-15 07:47:11 -0700

    write a program that prints _putchar on the screen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints the word simply "_putchar"
+ * on the screen
+ * Return: the function returns "0" when ran successfully
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
