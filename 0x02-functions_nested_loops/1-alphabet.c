cd8a393915955c781a884691e424bbd2b98ee146
Ok-michael <okomichaelaku@gmail.com>
2023-08-16 00:13:54 +0100

    treated the unterminated commit issue

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - print all the small letters
+ * print_alphabet - print all the small letters
  * Return: return 0 if everything goes on well
  */
 void print_alphabet(void)
@@ -11,5 +11,4 @@ void print_alphabet(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-	return;
 }
