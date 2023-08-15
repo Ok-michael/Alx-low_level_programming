93823bf8e67f669dcf7dd498a5dc90bd56b5e359
Ok-michael <okomichaelaku@gmail.com>
2023-08-15 19:24:22 +0100

    this file prints all the small letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print all the small letters
+ * Return: return 0 if everything goes on well
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	Return (0);
+}
