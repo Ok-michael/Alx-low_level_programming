7d64366fa05163d2211f381722df992823871fd9
Ok-michael <okomichaelaku@gmail.com>
2023-08-16 01:31:27 +0100

    Write a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 
 /**
  * _islower - checks if the character passed is a lower case character
+ * @Char: the function accepts one integer parameter Char and checks if it
+ * is lower
  * Return: returns a boolean True if the check is successfull
  * otherwise it returns False
  */
