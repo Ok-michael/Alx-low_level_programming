2b0f154c2b2acf8f14c3d7c0bd0b7118f798eee1
Ok-michael <okomichaelaku@gmail.com>
2023-08-16 16:35:48 +0100

    make changes to 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,7 +38,8 @@ void print_times_table(int k)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(prod / 10) % 10 + '0');
+					_putchar(prod / 100 + '0');
+					_putchar((prod / 10) % 10 + '0');
 					_putchar(prod % 10 + '0');
 				}
 			}
