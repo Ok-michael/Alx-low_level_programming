8059aa22dca5076435949a0063f4f9360109031f
De-invincible <theinvinciblevoicemagazine@gmail.com>
2023-08-17 21:15:37 +0100

    Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - check for a digit
+ * @size : integer type
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+
+	int p = 1, pp;
+
+	while (p <= size && size > 0)
+	{
+		pp = 0;
+		while (pp < size - p)
+		{
+			_putchar(' ');
+			pp++;
+		}
+		pp = 0;
+		while (pp < i)
+		{
+			_putchar('#');
+			pp++;
+		}
+		_putchar('\n');
+		p++;
+	}
+	if (p == 1)
+	_putchar('\n');
+}
