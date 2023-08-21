87ee54cb3b673da9206270e8a9f8ecccc0ed16c6
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-21 08:31:17 -0700

    create a directory, readme file and also a cfile that contains a function 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - this funtion assigns 98 to the pointer
  * it takes in as a parameter
- * @*n: this function accepts a pointer *n as
+ * @n: this function accepts a pointer *n as
  * Return: this function returns void
  */
 void reset_to_98(int *n)
