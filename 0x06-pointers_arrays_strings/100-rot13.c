9526f10d0ef91849aa7c00edb46d73b35f59d7e8
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-24 15:45:57 -0700

    hot fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,38 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * rot13 - this function take encripts a string using
- * rot13
- * @words: this function takes a string as input
- * Return: this function returns the encripted
- * string
+ * rot13 - this function encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *rot13(char *words)
+
+char *rot13(char *s)
 {
-	char *encr_word = (char *)malloc(strlen(words) + 1);
-	int i;
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	if (words == NULL)
-		return (NULL);
-	if (encr_word == NULL)
-		return (NULL);
-	for (i = 0; words[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		char c = words[i];
-
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		for (i = 0; i < 52; i++)
 		{
-			if (c >= 'a' && c <= 'z')
-				encr_word[i] = (((c - 'a') + 13) % 26) + 'a';
-			else
-				encr_word[i] = (((c - 'A') + 13) % 26) + 'A';
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
 		}
-		else
-			encr_word[i] = c;
+		count++;
 	}
-	encr_word[i] = '\0';
-	return (encr_word);
+
+	return (s);
 }
