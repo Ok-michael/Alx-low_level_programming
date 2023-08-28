0877f3b10c0d48e5ff371d963cc247b4dbf1c9bf
De-invincible <theinvinciblevoicemagazine@gmail.com>
2023-08-28 19:26:44 +0100

    fix 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * _strchr - this function locates a character in a string
- * passed in as an argument
- * @s: this function takes a string literal s as a parameter
- * @c: this function takes a character literal c as
- * a parameter
- * Return: a pointer to the first occurrence of
- * the character c
- * in the string s, or NULL if the character is not found
+ * *_strchr - this function fills memory with a constant byte.
+ * @s: it uses a pointer s to put the constant
+ * @c: this is the constant
+ * Return: s
  */
+
 char *_strchr(char *s, char c)
 {
-	int k = 0;
+	int i;
 
-	while (s[k] != '\0')
+	for (i = 0; s[i] >= '\0'  ; i++)
 	{
-		if (s[k] == c)
-			return (s + k);
-		k++;
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
 	}
+
 	return ('\0');
 }
