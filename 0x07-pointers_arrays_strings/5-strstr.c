8da7becf037b306a9f72882220e8cf1c19c44b18
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-28 10:34:34 -0700

    hot fix on strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,8 @@
  * _strstr - this function locates a substring
  * @haystack: this is the string where the search is made
  * @needle: pointer
- * Return: pointer to the beginning of the located substring, or NULL if the substring is not found.
+ * Return: pointer to the beginning of the located substrin
+ * or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
