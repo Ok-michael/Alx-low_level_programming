ede3dc195f74593a1b06cab9b39ba4e8bae96ed5
Ok-michael <okomichaelaku@gmail.com>
2023-08-28 16:45:17 +0100

    update 2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,19 @@
  * passed in as an argument
  * @s: this function takes a string literal s as a parameter
  * @c: this function takes a character literal c as a parameter
- * Return: a pointer to the first occurrence of the character c 
+ * Return: a pointer to the first occurrence of the character c
  * in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
 	int k = 0;
-	while(s[k] != '\0')
+
+	while (s[k] != '\0')
 	{
 		if (s[k] == c)
-			return (s[i]);
+			return (s +k);
+		k++;
 	}
-	return ('\0);
+	return ('\0');
 }
 
