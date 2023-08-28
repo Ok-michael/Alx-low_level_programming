efef26d5bfcf4ca0bc56ea346efd227880672ee0
Ok-michael <okomichaelakpu2@gmail.com>
2023-08-28 10:45:35 -0700

    modifiy string doc on 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * _strchr - this function locates a character in a string
  * passed in as an argument
  * @s: this function takes a string literal s as a parameter
  * @c: this function takes a character literal c as
  * a parameter
- * Return: a pointer to the first occurrence of 
+ * Return: a pointer to the first occurrence of
  * the character c
  * in the string s, or NULL if the character is not found
  */
