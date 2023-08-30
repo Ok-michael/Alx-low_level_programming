f4e60e6cb3aa35b14682949dd3f1c6a7e656a071
Ok-michael <okomichaelaku@gmail.com>
2023-08-30 13:00:09 +0100

    make 100-is_palindrome.c betty compliant

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * seedCompare - this function actually does the major
@@ -11,24 +11,25 @@
  * Return: this function return a boolean value depending
  * on the string passed
  */
-bool seedCompare(char*s, int LHstart, int RHstart)
+bool seedCompare(char *s, int LHstart, int RHstart)
 {
 	if (RHstart < LHstart)
 		return (1);
 	if (s[RHstart != LHstart)
 		return (0);
-	return seedCompare(s, ++LHstart, RHstart--);
+	return (seedCompare(s, ++LHstart, RHstart--));
 }
 
 /**
  * is_palindrome - this function checks if a passed string
  * is a palindrome
  * @s: this function takes a string s as input
- * Return: this function return and integer value 0 if 
+ * Return: this function return and integer value 0 if
  * the string is not a palindrom, otherwise it returns 1
  */
 int is_palindrome(char *s);
 {
 	int length = strlen(s);
-	return (seedCompare(s, 0, length -1));
+
+	return (seedCompare(s, 0, length - 1));
 }
