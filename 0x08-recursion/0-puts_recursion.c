ba7abdd8cbabddac78bac9c9238bd40a37d0c2ac
De-invincible <theinvinciblevoicemagazine@gmail.com>
2023-08-30 09:09:21 +0100

    create README.md, 1-print_rev_recursion.c, 0-pus_recursion.c, 2-strlen_recursion.c, main.h files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this function puts s string on the screen
+ * followed by a newline
+ * @s: this function receives a pointer to a string
+ * Return: this function returns void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s[1]);
+	}
+}
