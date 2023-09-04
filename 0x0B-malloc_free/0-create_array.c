3785f3b48b521e7929470ee5f4ea817ea4c3ac06
De-invincible <theinvinciblevoicemagazine@gmail.com>
2023-09-04 14:12:15 +0100

    Create main.h, README.md and 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - this function creates an array of characters
+ * and sets every entry with the specified character that was passed
+ * as an argument.
+ * @size: this is the size of the array passed during function
+ * invocation.
+ * @c: this is the character specified during the function invocation
+ * Return: pointer to the array initialized or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	int K = 0;
+
+	if (size == 0)
+		return NULL;
+	char *pntr = (char *)malloc(size*(sizeof(char));
+	if(pntr == NULL)
+		return NULL;
+	while (k < size)
+		pntr[k] = c;
+	return (pntr);
+}
