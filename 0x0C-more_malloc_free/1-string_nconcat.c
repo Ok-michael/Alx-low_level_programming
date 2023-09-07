e757af6b2ac60bb65c7114be9886587153076089
De-invincible <theinvinciblevoicemagazine@gmail.com>
2023-09-07 13:35:47 +0100

    fix a syntax error on 1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,13 +4,13 @@ int get_len(char *s)
 {
 	int k = 0;
 	while(s[k])
-		K++;
+		k++;
 	return k;
 }
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pntr;
-	unsigned int i = 0; k = 0,;
+	unsigned int i = 0, k = 0;
 
 	if(s1 == NULL)
 		s1 = "";
