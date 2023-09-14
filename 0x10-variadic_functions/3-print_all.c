d75933775ff77e1c40bcd489d3200eee0ce7da59
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-14 23:35:53 +0100

    heal 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,37 +1,43 @@
 #include "variadic_functions.h"
 
-/**
- * print_char - this function prints a character
- * @argument: the va_list with the character to print as it's next element
- * Return: this is the number of bytes printed
- */
-int print_char(va_list argument)
-{
-	return (printf("%c", va_arg(argument, int)));
-}
 
 /**
- * print_float - this function print a float
- * @argument: this is the va_list with the float to print as it's next element
+ * print_char - print a character
+ * @args: the va_list with the character to print as it's next element
+ *
  * Return: the number of bytes printed
  */
-int print_float(va_list argument)
+int print_char(va_list args)
 {
-	return (printf("%f", va_arg(argument, double)));
+	return (printf("%c", va_arg(args, int)));
 }
 
+
 /**
- * print_int - this function prints an integer
- * @argument: this is the va_list with the integer to print as it's next element
+ * print_float - this helper function print a float
+ * @args: the va_list with the float to print as it's next element
+ *
  * Return: the number of bytes printed
  */
-int print_int(va_list argument)
+int print_float(va_list args)
 {
-	return (printf("%i", va_arg(argument, int)));
+	return (printf("%f", va_arg(args, double)));
 }
 
+
 /**
- * print_str - this function prints a string
+ * print_int - this helper print an integer
+ * @args: the va_list with the integer to print as it's next element
+ * Return: the number of bytes printed
+ */
+int print_int(va_list args)
+{
+	return (printf("%i", va_arg(args, int)));
+}
+
+
+/**
+ * print_str - this function print a string
  * @args: the va_list with the string to print as it's next element
  * Return: the number of bytes printed
  */
@@ -44,14 +50,15 @@ int print_str(va_list args)
 	return (printf("%s", str));
 }
 
+
 /**
- * print_all - this function prints anything passed in as an argument
+ * print_all - it prints anything
  * @format: a format string listing the types of the proceeding arguments
  * @...: the values to print
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	va_list arguments;
 	print_fn_t fn_list[] = {
 		{'c', print_char},
 		{'f', print_float},
@@ -62,7 +69,7 @@ void print_all(const char * const format, ...)
 	char *sep[] = {"", ", "};
 	unsigned int bytes = 0, fn_index = 0, format_index = 0;
 
-	va_start(args, format);
+	va_start(arguments, format);
 	while (format && format[format_index])
 	{
 		fn_index = 0;
@@ -71,7 +78,7 @@ void print_all(const char * const format, ...)
 			if (format[format_index] == fn_list[fn_index].format)
 			{
 				printf("%s", sep[bytes != 0]);
-				bytes += fn_list[fn_index].fn(args);
+				bytes += fn_list[fn_index].fn(arguments);
 				break;
 			}
 			++fn_index;
@@ -79,5 +86,5 @@ void print_all(const char * const format, ...)
 		++format_index;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(arguments);
 }
