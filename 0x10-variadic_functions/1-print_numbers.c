f88c5661c5a9145ecd998d35796712ff86f3ea99
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-14 22:30:49 +0100

    fix 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (rem--)
 	{
 		printf("%i", va_arg(arguments, int));
-		if (remaining)
+		if (rem)
 			printf("%s", separator);
 	}
 	printf("\n");
