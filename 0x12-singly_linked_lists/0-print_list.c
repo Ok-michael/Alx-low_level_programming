8ba67aa63de8c70559be629f4cb1662dd9e2ec25
Michael Oko <okomichaelakpu2@gmail.com>
2023-09-21 17:33:33 +0100

    fix 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "lists.h>
+#include "lists.h"
 
 /**
- * print_list - this function prints elements of a singly linked
- * list on the standard output
- * @h:  this is the linked list to be printed
- * Return: this function returns the number of nodes
+ * print_list - this function prints elements of a linkedlist
+ * @h: this is the linked list passed
+ *
+ * Return: this function returns the number of elements passed
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *pter = h;
 	size_t nmb_nodez = 0;
-	
+	const list_t *pter = h;
+
 	while (pter != NULL)
 	{
 		printf("[%d] %s\n", pter->len,
-				pter->str != NULL ? pter->str: "(nil)");
+				pter->str != NULL ? pter->str : "(nil)");
 		pter = pter->next;
 		nmb_nodez++;
 	}
