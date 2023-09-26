1a97712c942ea561d09891ff30e32906d86657b3
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-26 15:25:46 +0100

    create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4_free_listint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_listint - this function frees the memories allocated
+ * @head: this is the pointer to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+        listint_t *tmpr;
+
+        if (head == NULL)
+        {
+                return (NULL);
+        }
+        while (head != NULL)
+        {
+                tmpr = head->next
+                free(head);
+                head = tempr;
+        }
+        free(head);
+}
