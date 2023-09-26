334b0371a2054c076a0c02831ce3d4f7218d1a26
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-26 22:11:07 +0100

    fix 102-free_listin_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,63 +3,62 @@
 #include <stdio.h>
 
 /**
- * _reallocator - this function reallocates memory for an array of
- * pointers to the nodes in a SLL
- * @list: this is the list to be appended to
- * @size: this is the size of the new list which is always one more
- * than the old list
+ * reallocate - this function reallocates memory for an array of
+ * pointers to the nodes in a linked list
+ * @list: tis is the old list to be appended
+ * @size: this is the size of the new list and it is always one
+ * more than the old list)
  * @new: this is the new node to add to the list
- * Return: pointer to the new list
+ * Return: it returns a pointer to the new list
  */
-listint_t **_reallocator(listint_t **list, size_t size, listint_t *new)
+listint_t **reallocate(listint_t **list, size_t size, listint_t *new)
 {
-        listint_t **new_list;
-        size_t k;
+	listint_t **new_list;
+	size_t q;
 
-        new_list = malloc(size * sizeof(listint_t *));
-        if (new_list == NULL)
-        {
-                free(list);
-                exit(98);
-        }
-        for (k = 0; k < size - 1; k++)
-                new_list[k] = list[k];
-        new_list[k] = new;
-        free(list);
-        return (new_list);
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (q = 0; q < size - 1; q++)
+		new_list[q] = list[q];
+	new_list[q] = new;
+	free(list);
+	return (new_list);
 }
 
 /**
- * free_listint_safe - this function frees a listint_t
- * linked list.
- * @head: this is a double pointer to the start of the list
- * Return: this funtion returns the number of nodes in the list
+ * free_listint_safe - this function frees a listint_t linked list.
+ * @head: this is a 2D-pointer to the start of the list
+ * Return: this function returns the number of nodes in the list
  */
 size_t free_listint_safe(listint_t **head)
 {
-        size_t k, nmb = 0;
-        listint_t **list = NULL;
-        listint_t *next;
+	size_t q, nmb = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-        if (head == NULL || *head == NULL)
-                return (nmb);
-        while (*head != NULL)
-        {
-                for (k = 0; k < nmb; k++)
-                {
-                        if (*head == list[k])
-                        {
-                                *head = NULL;
-                                free(list);
-                                return (nmb);
-                        }
-                }
-                nmb = nmb + 1;
-                list = _reallocator(list, nmb, *head);
-                next = (*head)->next;
-                free(*head);
-                *head = next;
-        }
-        free(list);
-        return (nmb);
+	if (head == NULL || *head == NULL)
+		return (nmb);
+	while (*head != NULL)
+	{
+		for (q = 0; q < nmb; q++)
+		{
+			if (*head == list[q])
+			{
+				*head = NULL;
+				free(list);
+				return (nmb);
+			}
+		}
+		nmb = nmb + 1;
+		list = reallocate(list, nmb, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (nmb);
 }
