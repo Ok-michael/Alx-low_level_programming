ea05f4e5505dc969647cba1b1649c8f139acd5b0
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-26 20:56:56 +0100

    Betty my codes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -14,20 +14,20 @@
  */
 const listint_t **_reallocator(const listint_t **list, size_t size, const listint_t *new)
 {
-	const listint_t **newlist;
-	size_t k;
+        const listint_t **newlist;
+        size_t k;
 
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
-	{
-		free(list);
-		exit(98);
-	}
-	for (k = 0; k < size - 1; k++)
-		newlist[k] = list[k];
-	newlist[k] = new;
-	free(list);
-	return (newlist);
+        newlist = malloc(size * sizeof(listint_t *));
+        if (newlist == NULL)
+        {
+                free(list);
+                exit(98);
+        }
+        for (k = 0; k < size - 1; k++)
+                newlist[k] = list[k];
+        newlist[k] = new;
+        free(list);
+        return (newlist);
 }
 
 /**
@@ -38,25 +38,25 @@ const listint_t **_reallocator(const listint_t **list, size_t size, const listin
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t k, nmb = 0;
-	const listint_t **list = NULL;
+        size_t k, nmb = 0;
+        const listint_t **list = NULL;
 
-	while (head != NULL)
-	{
-		for (k = 0; k < nmb; k++)
-		{
-			if (head == list[k])
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free(list);
-				return (nmb);
-			}
-		}
-		nmb = nmb + 1;
-		list = _reallocator(list, nmb, head);
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	free(list);
-	return (nmb);
+        while (head != NULL)
+        {
+                for (k = 0; k < nmb; k++)
+                {
+                        if (head == list[k])
+                        {
+                                printf("-> [%p] %d\n", (void *)head, head->n);
+                                free(list);
+                                return (nmb);
+                        }
+                }
+                nmb = nmb + 1;
+                list = _reallocator(list, nmb, head);
+                printf("[%p] %d\n", (void *)head, head->n);
+                head = head->next;
+        }
+        free(list);
+        return (nmb);
 }
