2746f9aed4994dae2d7dcebe26f9ff749a7df040
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-26 14:04:13 +0100

    create a README.md, lists.h, and 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+include "lists.h"
+
+/**
+ * print_listint - this function will print all the elements of a list.
+ * @h: this parameter represents the head of the list to be traversed.
+ * Return: this function return an integer representing the number of
+ * in a list
+ */
+size_t print_listint(const listint_t *h)
+{
+        size_t node_cntr = 0;
+
+        for (; h != NULL; h = h->next)
+        {
+                printf("%d\n", h->n);
+                node_cntr++;
+        }
+        return (node_cntr);
+}
