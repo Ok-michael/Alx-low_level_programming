978e9d535380b040baeb5d71b6bf806a0926fa74
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-27 01:16:11 +0100

    fix code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,37 +8,41 @@
  * @n: this the value to be added at  the new node
  * Return: this function returns a pointer to the new node
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int idx)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	if (head == NULL || *head == NULL)
-	{
-		return (-1);
-	}
+	listint_t *current;
+	listint_t *new;
 
-	listint_t *current = *head;
-	listint_t *temp;
-	unsigned int i;
+	if (head == NULL)
+		return (0);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	new->next = NULL;
+	new->n = n;
 
 	if (idx == 0)
 	{
-		*head = current->next;
-		free(current);
-		return (1);
+		new->next = *head;
+		(*head) = new;
+		return (new);
 	}
 
-	for (i = 0; current != NULL && i < idx - 1; i++)
+	current = *head;
+
+	while (idx != 1)
 	{
 		current = current->next;
+		--idx;
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
 	}
+	new->next = current->next;
+	current->next = new;
 
-	if (current == NULL || current->next == NULL)
-	{
-		return (-1);
-	}
-
-	temp = current->next;
-	current->next = temp->next;
-	free(temp);
-
-	return (1);
+	return (new);
 }
