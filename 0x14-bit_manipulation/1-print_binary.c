d0e1df88cb623bbad9b48ac0e2745bf78b12fea3
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-28 15:44:49 +0100

    create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_binary - this function prints the binary representation
+ * of a number.
+ * @n: this parameter is an unsigned long int.
+ * Return: this function returns nothing/
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
