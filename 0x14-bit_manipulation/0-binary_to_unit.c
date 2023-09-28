8626d88770301549a3274002b1b85952b158ae6e
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-28 14:12:39 +0100

    create a README.md, main.h and 0-binary_to_unit.c

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - this function converts a binary number to anunsigned
+ * integer
+ * @b: this is the binary value to be converted.
+ *
+ * Return: this function returns the converted unsigned integer value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int length = 0, base_two = 1;
+	unsigned int uns_int = 0;
+
+	if (if b == 0)
+		return (0);
+	while (b[length])
+		length ++;
+	length--;
+	while (length >= 0)
+	{
+		length--;
+		base_two = base_two * 2;
+		if (b[length] != '0' && b[length] != '1')
+		{
+			return (0);
+		}
+		if (b[length] & 1)
+		{
+			uns_int += base_two;
+		}
+	}
+	return (uns_int);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,4 @@
+# Bit manipulation
+
+this directory demonstrate some key concepts in bit manipulation
+and it's application programmatically
