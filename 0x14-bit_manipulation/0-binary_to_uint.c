1bc9bca15d15932f8272a9d73fceb23ea5e59e3a
Ok-michael <okomichaelakpu2@gmail.com>
2023-09-28 17:39:09 +0100

    fix 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,32 +7,31 @@
  *
  * Return: this function returns the converted unsigned integer value
  */
-unsigned int binary_to_uint(const char *b)
+igned int binary_to_uint(const char *b)
 {
-	int length = 0, base_two = 1;
-	unsigned int uns_int = 0;
+	unsigned int uns_int;
+	int length, base_two;
 
 	if (!b)
 		return (0);
-	if (!strcmp(b, "1"))
-		return (1);
-	while (b[length])
-		length++;
-	length--;
-	while (length > 0)
+
+	uns_int = 0;
+
+	for (length = 0; b[length] != '\0'; length++)
+		;
+
+	for (length--, base_two = 1; length >= 0; length--, base_two *= 2)
 	{
-		length--;
-		base_two = base_two * 2;
 		if (b[length] != '0' && b[length] != '1')
 		{
 			return (0);
 		}
+
 		if (b[length] & 1)
 		{
 			uns_int += base_two;
 		}
-		/*length--;*/
-		/*base_two = base_two * 2;*/
 	}
+
 	return (uns_int);
 }
